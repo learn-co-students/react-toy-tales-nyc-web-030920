a16516d831270dfd62fee1067ea6de9103ca88cb
JohnsonNYC <jkow95@gmail.com>
2020-05-18 17:42:07 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ import ToyContainer from './components/ToyContainer'
 
 import data from './data'
 
-
-class App extends React.Component{
+let newData = data 
+class App extends React.Component {
 
   state = {
-    display: false
+    display: false,
+    toys: [],
+    name: '',
+    img: ''
   }
-
+  componentDidMount() {
+    this.setState({
+      toys: data
+    })
+  }
   handleClick = () => {
     let newBoolean = !this.state.display
     this.setState({
@@ -21,20 +28,43 @@ class App extends React.Component{
     })
   }
 
-  render(){
+  updateInput = (event) => {
+    // if name = name ss for name else
+    // if name = image ss for image
+    if (event.target.name === 'name') {
+      this.setState({ name: event.target.value })
+    } else if (event.target.name === 'image') {
+      this.setState({ img: event.target.value });
+    }
+  }
+  toggleSubmit = (event) => {
+    event.preventDefault();
+    let newToy = { id: this.state.toys.size + 1, name: this.state.name, image: this.state.img, likes: 0 }
+    this.setState({
+      toys: [...this.state.toys, newToy]
+    })
+  }
+
+  toggleDonate = (id) => {
+    let newArray =this.state.toys.filter(toy => toy.id != id)
+    this.setState({toys: newArray})
+    // newData = this.state.toys
+    }
+  
+  render() {
     return (
       <>
-        <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
+        <Header />
+        {this.state.display
+          ?
+          <ToyForm name={this.state.name} image={this.state.img} updateInput={this.updateInput} toggleSubmit={this.toggleSubmit} />
+          :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} donate={this.toggleDonate}/>
       </>
     );
   }
