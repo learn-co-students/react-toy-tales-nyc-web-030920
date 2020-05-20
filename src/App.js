c4f605af70c7f2bb4fc705fd44064eaf5b68dcb0
Samanthaponce5 <samanthaponce5@gmail.com>
2020-05-19 21:42:04 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ import data from './data'
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys:[],
+    name:'',
+    image:'',
+    likes:0
+
+
   }
 
   handleClick = () => {
@@ -21,20 +27,60 @@ class App extends React.Component{
     })
   }
 
+
+
+handleChange=(e)=>{
+  const{name,value}=e.target
+  this.setState({[name]:value})
+}
+
+getToys=()=>{
+  fetch(' http://localhost:3000/toys')
+  .then((resp)=>resp.json())
+  .then((data)=>this.setState({toys:data}))
+}
+
+componentDidMount(){
+  this.getToys()
+}
+
+handleSubmit=(e)=>{
+  e.preventDefault()
+const{name,image,likes}=this.state
+  fetch(' http://localhost:3000/toys',{
+    method:'POST',
+    headers:{
+      'Accept':'application/json',
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify({
+      name,
+      image,
+      likes
+    })
+  })
+.then(this.getToys)
+}
+
+
+
+
   render(){
+   
     return (
+
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm  {...this.state} handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer handleClicklikes={this.handleClicklikes} {...this.state} getToys={this.getToys}/>
       </>
     );
   }
